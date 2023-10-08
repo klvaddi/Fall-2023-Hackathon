c4b4fc27be2fe8322314531fb518fd675e0b22f4
UltimateSimon7 <115137925+UltimateSimon7@users.noreply.github.com>
2023-10-07 23:26:56 -0400

    push stuff

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,10 +1,15 @@
 const x = document.getElementById("demo");
 
 function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+  const confirmation = confirm("Allow this website to access your location?");
+  if (confirmation) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    x.innerHTML = "Geolocation access denied.";
   }
 }
 
