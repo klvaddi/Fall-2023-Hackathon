51704026b5b3023ce147832867100b514ab69c50
lokesh.vaddi <klvaddi@gmail.com>
2023-10-07 23:45:31 -0400

    update

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,7 +1,7 @@
 const x = document.getElementById("demo");
 
 function getLocation() {
-  latitude 
+  latitude;
   const confirmation = confirm("Allow this website to access your location?");
   if (confirmation) {
     if (navigator.geolocation) {
@@ -15,12 +15,9 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  
-  /*
   x.innerHTML =
     "Latitude: " +
     position.coords.latitude +
     "<br>Longitude: " +
     position.coords.longitude;
-  *
 }
