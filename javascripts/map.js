758a1473f78fd84d62d0bd8f2d55fbfe1b5cca8f
lokesh.vaddi <klvaddi@gmail.com>
2023-10-08 00:39:48 -0400

    update

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,23 +1,25 @@
 const x = document.getElementById("demo");
 
-function getLocation() {
-  latitude;
-  const confirmation = confirm("Allow this website to access your location?");
-  if (confirmation) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-  } else {
-    x.innerHTML = "Geolocation access denied.";
-  }
-}
+      function getLocation() {
+        const confirmation = confirm(
+          "Allow this website to access your location?"
+        );
+        if (confirmation) {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+          } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+          }
+        } else {
+          x.innerHTML = "Geolocation access denied.";
+        }
+      }
 
-function showPosition(position) {
-  x.innerHTML =
-    "Latitude: " +
-    position.coords.latitude +
-    "<br>Longitude: " +
-    position.coords.longitude;
-}
+      function showPosition(position) 
+      {
+          x.innerHTML =
+          "Latitude: " +
+          position.coords.latitude +
+          "<br>Longitude: " +
+          position.coords.longitude;
+      }    
