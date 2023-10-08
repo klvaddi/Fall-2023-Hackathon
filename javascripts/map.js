324481cb55cc9381665b41ae66080d2fc2ef65eb
UltimateSimon7 <115137925+UltimateSimon7@users.noreply.github.com>
2023-10-08 01:57:21 -0400

    push

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,45 +1,47 @@
-const x = document.getElementById("demo");
 function getLocation() {
   const confirmation = confirm("Allow this website to access your location?");
+  const mapContainer = document.getElementById("map-container");
+  const button = document.querySelector(".red-button");
+  const status = document.getElementById("status");
+
   if (confirmation) {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          // Hide the button and show the map container
+          button.style.display = "none";
+          mapContainer.style.display = "block";
+
+          // Initialize the map centered at the specified latitude and longitude
+          const map = new google.maps.Map(
+            document.getElementById("map-container"),
+            {
+              center: { lat: latitude, lng: longitude },
+              zoom: 15, // Zoom level (adjust as needed)
+            }
+          );
+
+          // Add a marker at the specified location
+          const marker = new google.maps.Marker({
+            position: { lat: latitude, lng: longitude },
+            map: map,
+            title: "Your Location", // Marker tooltip (optional)
+          });
+
+          // Update status message
+          status.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+        },
+        function () {
+          status.textContent = "Unable to retrieve your location";
+        }
+      );
     } else {
-      x.innerHTML = "Geolocation is not supported by this browser.";
+      status.textContent = "Geolocation is not supported by this browser";
     }
   } else {
-    x.innerHTML = "Geolocation access denied.";
+    status.textContent = "Geolocation access denied.";
   }
-}
-
-function showPosition(position) {
-  x.innerHTML =
-    "Latitude: " +
-    position.coords.latitude +
-    "<br>Longitude: " +
-    position.coords.longitude;
-
-  initMap(position.coords.latitude, position.coords.longitude);
-}
-function initMap(latitude, longitude) {
-  // Get latitude and longitude from geolocation
-  const lat = latitude; /* Your latitude value */
-  const lng = longitude; /* Your longitude value */
-
-  // Create a new map centered at the specified latitude and longitude
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: lat,
-      lng: lng,
-    },
-    zoom: 13,
-    mapTypeId: "roadmap",
-  });
-
-  // Add a marker at the specified location
-  const marker = new google.maps.Marker({
-    position: { lat: lat, lng: lng },
-    map: map,
-    title: "Your Location", // Marker tooltip (optional)
-  });
 }
