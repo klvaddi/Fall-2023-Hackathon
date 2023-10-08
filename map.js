9c97e49394d3507212f9f715ef74588bad7e8ad4
Istiaqul Alam <138840440+IstiaqulAlam@users.noreply.github.com>
2023-10-07 23:52:23 -0400

    Add files via upload

---
+++ map.js
@@ -0,0 +1,25 @@
+const x = document.getElementById("demo");
+
+      function getLocation() {
+        const confirmation = confirm(
+          "Allow this website to access your location?"
+        );
+        if (confirmation) {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+          } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+          }
+        } else {
+          x.innerHTML = "Geolocation access denied.";
+        }
+      }
+
+      function showPosition(position) 
+      {
+        x.innerHTML =
+          "Latitude: " +
+          position.coords.latitude +
+          "<br>Longitude: " +
+          position.coords.longitude;
+      }    
