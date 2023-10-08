1faea9791a589b4a4a9e37ab09ea1abe82a9b106
Hemanka <hkanoujia02@gmail.com>
2023-10-08 02:41:28 -0400

    register api

---
+++ api/register.php
@@ -0,0 +1,16 @@
+<?php
+
+    include 'function.php';
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+        $json_data = file_get_contents('php://input');
+        $post_data = json_decode($json_data, true);
+
+        $pre_login = new PreLogin();
+        $result = $pre_login->register_user($post_data);
+
+        echo json_encode($result, JSON_PRETTY_PRINT);
+    }
+
+?>
