fb7050aef0d93e798f304ee3a7ac240661b90c98
UltimateSimon7 <115137925+UltimateSimon7@users.noreply.github.com>
2023-10-07 22:12:08 -0400

    stuff

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,29 +1,17 @@
-function geoFindMe() {
-  const status = document.querySelector("#status");
-  const mapLink = document.querySelector("#map-link");
+const x = document.getElementById("demo");
 
-  mapLink.href = "";
-  mapLink.textContent = "";
-
-  function success(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    status.textContent = "";
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-  }
-
-  function error() {
-    status.textContent = "Unable to retrieve your location";
-  }
-
-  if (!navigator.geolocation) {
-    status.textContent = "Geolocation is not supported by your browser";
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
   } else {
-    status.textContent = "Locating…";
-    navigator.geolocation.getCurrentPosition(success, error);
+    x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
-document.querySelector("#find-me").addEventListener("click", geoFindMe);
+function showPosition(position) {
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
